91d63283ab04b96063a77e5d88354870c719068b
zccz14 <zheng.chen@no-trade-no.life>
2026-07-06 00:28:10 +0800

    💄 center email OTP input

--- ui-web/src/components/ui/input-otp.tsx
+++ ui-web/src/components/ui/input-otp.tsx
@@ -9,7 +9,7 @@ const InputOTP = React.forwardRef<
   <OTPInput
     ref={ref}
     containerClassName={cn(
-      'flex items-center gap-2 has-[:disabled]:opacity-50',
+      'flex items-center justify-center gap-2 has-[:disabled]:opacity-50',
       containerClassName,
     )}
     className={cn('disabled:cursor-not-allowed', className)}
